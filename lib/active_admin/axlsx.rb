c372235c6380b6e936df0325cefd0ec333ebbd2a
Thomas Himbert <thimbert@avarteq.de>
2015-10-30 14:52:47 +0100

    Fix initializer to get it work in staging and production

--- lib/active_admin/axlsx.rb
+++ lib/active_admin/axlsx.rb
@@ -18,7 +18,7 @@ class Railtie < ::Rails::Railtie
 
   end
 
-  config.after_initialize do
+  initializer "active_admin_axlsx_railtie.configure_rails_initialization" do |variable|
     ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::Axlsx::DSL
     ActiveAdmin::Resource.send :include, ActiveAdmin::Axlsx::ResourceExtension
     ActiveAdmin::Views::PaginatedCollection.add_format :xlsx
